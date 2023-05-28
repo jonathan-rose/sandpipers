fd8f3e05eb75295adf12bd88c034cc5d7ef7d5ec
Kimbsy <lordkimber@gmail.com>
2023-05-28 21:34:06 +0100

    Add beach to level-01
    getting things in place to make it easier to work in both scenes

--- src/sandpipers/scenes/level_01.clj
+++ src/sandpipers/scenes/level_01.clj
@@ -1,25 +1,27 @@
 (ns sandpipers.scenes.level-01
   (:require [quip.sprite :as qpsprite]
-            [quip.utils :as qpu]))
-
-(def light-green [133 255 199])
+            [quip.tween :as qptween]
+            [quip.utils :as qpu]
+            [sandpipers.common :as common]
+            [sandpipers.sprites.beach :as beach]))
 
 (defn sprites
   "The initial list of sprites for this scene"
   []
-  [])
+  [(beach/beach)])
 
 (defn draw-level-01
   "Called each frame, draws the current scene to the screen"
   [state]
-  (qpu/background light-green)
+  (qpu/background common/sky-blue)
   (qpsprite/draw-scene-sprites state))
 
 (defn update-level-01
   "Called each frame, update the sprites in the current scene"
   [state]
   (-> state
-      qpsprite/update-scene-sprites))
+      qpsprite/update-scene-sprites
+      qptween/update-sprite-tweens))
 
 (defn init
   "Initialise this scene"
