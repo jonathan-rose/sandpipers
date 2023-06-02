39771bea2ef0a2bda6229d5ac809e26cae8163bc
jonathan-rose <jonathan_rose@outlook.com>
2023-06-02 09:27:08 +0100

    Placeholder sandpiper art and testing keyboard input with rotation tween.

--- src/sandpipers/scenes/level_01.clj
+++ src/sandpipers/scenes/level_01.clj
@@ -1,13 +1,27 @@
 (ns sandpipers.scenes.level-01
   (:require [quip.sprite :as qpsprite]
-            [quip.utils :as qpu]))
+            [quip.utils :as qpu]
+            [quip.tween :as qptween]))
 
 (def light-green [133 255 199])
+
+(defn animated-sandpiper
+  [pos current-animation]
+  (qpsprite/animated-sprite
+   :player
+   pos
+   128
+   128
+   "sandpiper-spritesheet.png"
+   :animations {:idle {:frames 1
+                       :y-offset 0
+                       :frame-delay 15}}
+   :current-animation current-animation))
 
 (defn sprites
   "The initial list of sprites for this scene"
   []
-  [])
+  [(animated-sandpiper [100 100] :idle)])
 
 (defn draw-level-01
   "Called each frame, draws the current scene to the screen"
@@ -19,11 +33,28 @@
   "Called each frame, update the sprites in the current scene"
   [state]
   (-> state
-      qpsprite/update-scene-sprites))
+      qpsprite/update-scene-sprites
+      qptween/update-sprite-tweens))
+
+(defn tween-sandpiper
+  [state rotation]
+  (update-in state
+             [:scenes :level-01 :sprites]
+             (fn [sprites]
+               [(qptween/add-tween
+                 (first sprites)
+                 (qptween/->tween :rotation rotation))])))
+
+(defn handle-key-pressed
+  [state e]
+  (case (:key e)
+    :left (tween-sandpiper state 90)
+    :right (tween-sandpiper state -90) state))
 
 (defn init
   "Initialise this scene"
   []
   {:sprites (sprites)
    :draw-fn draw-level-01
-   :update-fn update-level-01})
+   :update-fn update-level-01
+   :key-pressed-fns [handle-key-pressed]})
