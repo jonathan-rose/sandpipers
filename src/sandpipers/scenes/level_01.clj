c737c3c2bbad924f4acce424d76c141abf896ad8
jonathan-rose <jonathan_rose@outlook.com>
2023-06-03 18:49:31 +0100

    Sandpaper can peck the ground and minor movement value tweaks.
    Press 'Down' to peck the ground. Values for min and max speed have
    been tweaked to create nippier movement. Frame delay decreased to
    create sense of more frenetic legs.

--- src/sandpipers/scenes/level_01.clj
+++ src/sandpipers/scenes/level_01.clj
@@ -13,18 +13,21 @@
           128
           128
           "sandpiper-spritesheet.png"
-          :animations {:idle     {:frames 1
-                                  :y-offset 0
-                                  :frame-delay 1}
-                       :run-left {:frames 3
-                                  :y-offset 1
-                                  :frame-delay 10}
+          :animations {:idle      {:frames 1
+                                   :y-offset 0
+                                   :frame-delay 1}
+                       :run-left  {:frames 3
+                                   :y-offset 1
+                                   :frame-delay 5}
                        :run-right {:frames 3
                                    :y-offset 2
+                                   :frame-delay 5}
+                       :eat       {:frames 1
+                                   :y-offset 3
                                    :frame-delay 10}}
           :current-animation current-animation)
-          :max-speed 5
-          :min-speed -5))
+          :max-speed 12
+          :min-speed -12))
 
 (defn sprites
   "The initial list of sprites for this scene"
@@ -42,7 +45,7 @@
        (fn [s]
          (-> s
              (update :vel (fn [[xvel yvel]]
-                            [(max (:min-speed s) (- xvel 0.2)) yvel]))
+                            [(max (:min-speed s) (- xvel 0.5)) yvel]))
              ((fn [sprite]
                 (if (not= (get sprite :current-animation) :run-left)
                   (qpsprite/set-animation sprite :run-left)
@@ -55,11 +58,20 @@
        (fn [s]
          (-> s
              (update :vel (fn [[xvel yvel]]
-                            [(min (:max-speed s) (+ xvel 0.2)) yvel]))
+                            [(min (:max-speed s) (+ xvel 0.5)) yvel]))
              ((fn [sprite]
                 (if (not= (get sprite :current-animation) :run-right)
                   (qpsprite/set-animation sprite :run-right)
                   sprite))))))
+
+      (currently-held? :down)
+      (qpsprite/update-sprites-by-pred
+       state
+       (qpsprite/group-pred :player)
+       (fn [s]
+         (-> s
+             (update :vel (fn [[xvel yvel]] [0 yvel]))
+             (qpsprite/set-animation :eat))))
 
       :else
       (qpsprite/update-sprites-by-pred
